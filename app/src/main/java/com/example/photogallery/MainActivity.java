42da325341ba02bb6e04c2f2bd2f2aff98edc2b3
System Administrator <root@macbook-air-3.wifi.bcit.ca>
2019-10-02 18:59:18 -0700

    Add button navigation

--- app/src/main/java/com/example/photogallery/MainActivity.java
+++ app/src/main/java/com/example/photogallery/MainActivity.java
@@ -7,13 +7,26 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     private DrawerLayout drawer;
+    private int currentPhotoIndex = 0;
+    private String currentPhotoPath = null;
+    private ArrayList<String> photoGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,20 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
                     new PhotosFragment()).commit();
             navView.setCheckedItem(R.id.nav_photos);
         }
+
+        //Initialize navigation buttons
+        Button buttonLeft = findViewById(R.id.buttonLeft);
+        Button buttonRight = findViewById(R.id.buttonRight);
+        buttonLeft.setOnClickListener(this);
+        buttonRight.setOnClickListener(this);
+
+        Date minDate = new Date(Long.MIN_VALUE);
+        Date maxDate = new Date(Long.MAX_VALUE);
+        photoGallery = populateGallery(minDate, maxDate);
+        Log.d("onCreate, size", Integer.toString(photoGallery.size()));
+        if (photoGallery.size() > 0)
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        displayPhoto(currentPhotoPath);
     }
 
     @Override
@@ -67,5 +94,46 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         else {
             super.onBackPressed();
         }
+    }
+
+    private ArrayList<String> populateGallery(Date minDate, Date maxDate) {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/app/src/main/res/drawable");
+        photoGallery = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : file.listFiles()) {
+                photoGallery.add(f.getPath());
+            }
+        }
+        return photoGallery;
+    }
+
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.ivMain);
+        iv.setImageBitmap(BitmapFactory.decodeFile(path));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLeft:
+                --currentPhotoIndex;
+                break;
+            case R.id.buttonRight:
+                ++currentPhotoIndex;
+                break;
+            default:
+                break;
+        }
+        if (currentPhotoIndex < 0)
+            currentPhotoIndex = 0;
+        if (currentPhotoIndex >= photoGallery.size())
+            currentPhotoIndex = photoGallery.size() - 1;
+
+        currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        Log.d("phpotoleft, size", Integer.toString(photoGallery.size()));
+        Log.d("photoleft, index", Integer.toString(currentPhotoIndex));
+        displayPhoto(currentPhotoPath);
     }
 }
