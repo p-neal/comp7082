5a6ba1d179f90b168997fffdb78aee008ebe1013
Sashaye <sasroye@gmail.com>
2019-10-15 20:11:16 -0700

    navigation and fragment

--- app/src/main/java/com/example/photogallery/AlbumsFragment.java
+++ app/src/main/java/com/example/photogallery/AlbumsFragment.java
@@ -1,19 +1,100 @@
 package com.example.photogallery;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class AlbumsFragment extends Fragment {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AlbumsFragment extends Fragment implements View.OnClickListener {
+
+    /*@Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_albums, container, false);
+    }*/
+
+    private ArrayList<String> photoGallery;
+    private String currentPhotoPath = null;
+    private int currentPhotoIndex = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_albums, container, false);
+        //View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_albums, container, false);
+
+        Button buttonLeft = (Button) rootView.findViewById(R.id.buttonLeft);
+        Button buttonRight = (Button) rootView.findViewById(R.id.buttonRight);
+
+        buttonLeft.setOnClickListener(this);
+        buttonRight.setOnClickListener(this);
+
+        Date minDate = new Date(Long.MIN_VALUE);
+        Date maxDate = new Date(Long.MAX_VALUE);
+
+        photoGallery = populateGallery(minDate, maxDate);
+        Log.d("onCreate, size", Integer.toString(photoGallery.size()));
+        if (photoGallery.size() > 0)
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        displayPhoto(currentPhotoPath, rootView);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLeft:
+                --currentPhotoIndex;
+                break;
+            case R.id.buttonRight:
+                ++currentPhotoIndex;
+                break;
+            default:
+                break;
+        }
+        if (currentPhotoIndex < 0)
+            currentPhotoIndex = 0;
+        if (currentPhotoIndex >= photoGallery.size())
+            currentPhotoIndex = photoGallery.size() - 1;
+
+        currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        Log.d("phpotoleft, size", Integer.toString(photoGallery.size()));
+        Log.d("photoleft, index", Integer.toString(currentPhotoIndex));
+        displayPhoto(currentPhotoPath, view);
+    }
+
+    private ArrayList<String> populateGallery(Date minDate, Date maxDate) {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(),"/Android/data/com.example.photogallery/files/Pictures");
+
+        photoGallery = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : file.listFiles()) {
+                photoGallery.add(f.getPath());
+            }
+        }
+        Log.d("photogallery", Integer.toString(fList.length));
+        return photoGallery;
+    }
+
+    private void displayPhoto(String path, View view) {
+        ImageView iv = (ImageView) view.findViewById(R.id.ivMain);
+        iv.setImageBitmap(BitmapFactory.decodeFile(path));
     }
 }
